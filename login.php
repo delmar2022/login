868a0ef4ebf9f5a5c70f8059462d178a8b877a44
delmar2022 <softwysop@gmail.com>
2022-03-26 08:42:14 -0600

    Correccion de archivos

--- login.php
+++ login.php
@@ -2,21 +2,23 @@
 include('config.php');
 session_start();
 if (isset($_POST['login'])) {
+
     $username = $_POST['usuario'];
     $password = $_POST['password'];
 
-    $query = $conn->prepare("SELECT * FROM usuarios WHERE username = :username");
+    $query = $conn->prepare("SELECT * FROM usuarios WHERE username=:username");
     $query->bindParam("username", $username, PDO::PARAM_STR);
     $query->execute();
-
     $result = $query->fetch(PDO::FETCH_ASSOC);
+
     if (!$result) {
-        echo '¡La combinación de nombre de usuario y contraseña es incorrecta! x';
+        echo '<p>¡La combinación de nombre de usuario y contraseña es incorrecta! x</p>';
     } else {
         if (password_verify($password, $result['password'])) {
-            echo '¡Felicitaciones, estás registrado!';
+            $_SESSION['user_id'] = $result['id'];
+            echo '<p>¡Felicitaciones, estás registrado!</p>';
         } else {
-            echo '¡La combinación de nombre de usuario y contraseña es incorrecta! y';
+            echo '<p>¡La combinación de nombre de usuario y contraseña es incorrecta! y</p>';
         }
     }
 }
--- registro.php
+++ registro.php
@@ -1,28 +1,33 @@
 <?php
+
 include('config.php');
 session_start();
+
 if (isset($_POST['registro'])) {
+
     $username = $_POST['usuario'];
     $email = $_POST['email'];
     $password = $_POST['password'];
     $password_hash = password_hash($password, PASSWORD_BCRYPT);
-
-    $query = $conn->prepare("SELECT * FROM usuarios WHERE email =:email");
+    $query = $conn->prepare("SELECT * FROM usuarios WHERE email=:email");
     $query->bindParam("email", $email, PDO::PARAM_STR);
     $query->execute();
+
     if ($query->rowCount() > 0) {
-        echo "El Correo esta registrado";
+        echo '<p>¡La dirección de correo electrónico ya está registrada!</p>';
     }
+
     if ($query->rowCount() == 0) {
         $query = $conn->prepare("INSERT INTO usuarios(username,password,email) VALUES (:username,:password_hash,:email)");
         $query->bindParam("username", $username, PDO::PARAM_STR);
-        $query->bindParam("password_hash", $password, PDO::PARAM_STR);
+        $query->bindParam("password_hash", $password_hash, PDO::PARAM_STR);
         $query->bindParam("email", $email, PDO::PARAM_STR);
         $result = $query->execute();
+
         if ($result) {
-            echo " Registro exitoso";
+            echo '<p>¡Tu registro fue exitoso!</p>';
         } else {
-            echo "Registro fallido";
+            echo '<p class="error">¡Algo salió mal!</p>';
         }
     }
 }
